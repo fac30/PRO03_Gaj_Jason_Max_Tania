7d1bb70572a3ef9ee5d80335140426e49eef0375
Jason Warren <contact.jwarren@gmail.com>
2024-09-25 13:06:41 +0100

    Basic route differentiation

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,26 @@
-import express, { Request, Response } from "express";
+import express, { Express, Request, Response } from "express";
 import * as dotenv from "dotenv";
-/* const express = require("express"); */
-/* const dotenv = require("dotenv"); */
+
 dotenv.config();
 
-const keys = {
-    port: process.env.PORT
-}
+const keys = { port: process.env.PORT }
+const app: Express = express();
 
-const app = express();
-
-app.get('/', (req: Request, res: Response) => {
-  res.send({ message: "OI OI" });
+//h1 Routers
+app.get('/openai', (req: Request, res: Response) => {
+  res.send({ message: "Server --> OpenAI" });
 });
 
+app.get('/spotify', (req: Request, res: Response) => {
+  res.send({ message: "Server --> Spotify" });
+});
+
+//h2 Fallback
+app.get('/', (req: Request, res: Response) => {
+  res.send({ message: "Server --> Server" });
+});
+
+//h1 Listeners
 app.listen(keys.port, () => {
   console.log(`Server running on port ${keys.port}`);
 });
