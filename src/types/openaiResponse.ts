ebcf328cc49e43ecc6e3b97300fc6efa46ad1fac
maxitect <maxime.downe@gmail.com>
2024-09-26 12:02:35 +0100

    fix: openaiResponse interface to include spotifyFeatures

--- src/types/openaiResponse.ts
+++ src/types/openaiResponse.ts
@@ -1,4 +1,7 @@
+import { spotifyFeatures } from "./spotifyQuery.js";
+
 export interface openaiResponse {
     mood: string,
-    genre: string
+    genre: string,
+    spotifyFeatures: spotifyFeatures
 };
