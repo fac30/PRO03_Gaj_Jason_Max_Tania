ced7450a6bbcde646ad0fdb50c8f83e14e464a71
maxitect <maxime.downe@gmail.com>
2024-09-30 21:33:14 +0100

    feat: treat date item as a date type and add playlistCount key to set number of songs

--- src/controllers/collateData.ts
+++ src/controllers/collateData.ts
@@ -1,9 +1,10 @@
 import { SpotifyQuery } from '../types/spotifyQuery';
 import { OpenAIResponse } from '../types/openaiResponse';
 
-export function collateData(features: OpenAIResponse, date: Date): SpotifyQuery {
+export function collateData(features: OpenAIResponse, date: string, playlistCount: number): SpotifyQuery {
     return {
         ...features,
-        date: date.toISOString().split('T')[0]
+        date: new Date(date),
+        playlistCount: playlistCount
     };
 }
